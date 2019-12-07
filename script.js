30f6261fdd6b2d57e673a0d5d1c2e32b22674907
lisahall1085 <54725092+lisahall1085@users.noreply.github.com>
2019-12-07 13:32:36 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var name = window.prompt("Please enter your name:");
+window.alert("Hello, " + name + "! How are you today?");
+console.log("Hello, " + name + "! How are you today?"); 
+var userName = window.prompt('Please enter your name:','default');
+
+window.alert('Hello, '+ userName);
+
+console.log('Hello, '+ userName);
